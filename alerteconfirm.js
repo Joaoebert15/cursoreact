1d3a8fa0fec1d72e4b1e5c31790149f8348b3d09
Ebertzss <105881899+Joaoebert15@users.noreply.github.com>
2023-05-07 02:44:03 -0300

    Add files via upload

---
+++ alerteconfirm.js
@@ -0,0 +1,5 @@
+const num1 = parseFloat(prompt('digite o primeiro numero'))
+const num2 = parseFloat(prompt('digte o segundo numero'))
+
+document.write(num1 + num2)
+console.log(num1 + num2)
---
+++ operadores.js
@@ -0,0 +1,12 @@
+/* aritmeticos 
+   + Adiçao e concatenaçao
+   - / *
+*/
+
+//NaN
+
+      const num1 = 10;
+      const num2 = parseFloat('5.2')
+
+      console.log(num1 + num2)
+      
